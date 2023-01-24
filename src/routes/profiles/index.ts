bf490519a62d7b56b008384adbfd2b6b1735503c
Mitya <partyzan327@gmail.com>
2023-01-24 22:22:07 +0300

    feat: implement profiles/get methods

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -1,55 +1,70 @@
-// import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-// import { idParamSchema } from '../../utils/reusedSchemas';
-// import { createProfileBodySchema, changeProfileBodySchema } from './schema';
-// import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
+import { Constants } from './../../utils/constants';
+import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { idParamSchema } from '../../utils/reusedSchemas';
+import {
+	//createProfileBodySchema,
+	//changeProfileBodySchema
+ } from './schema';
+import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
 
-// const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
-// 	fastify
-// ): Promise<void> => {
-// 	fastify.get('/', async function (request, reply): Promise<
-// 		ProfileEntity[]
-// 	> {});
+const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
+	fastify
+): Promise<void> => {
+	fastify.get('/', async function (request, reply): Promise<
+		ProfileEntity[]
+	> {
+		return reply.send(this.db.profiles);
+	});
 
-// 	fastify.get(
-// 		'/:id',
-// 		{
-// 			schema: {
-// 				params: idParamSchema,
-// 			},
-// 		},
-// 		async function (request, reply): Promise<ProfileEntity> {}
-// 	);
+	fastify.get(
+		'/:id',
+		{
+			schema: {
+				params: idParamSchema,
+			},
+		},
+		async function (request, reply): Promise<ProfileEntity> {
+			const { id } = request.params;
+			const profile = await this.db.profiles.findOne({ key: 'id', equals: id });
 
-// 	fastify.post(
-// 		'/',
-// 		{
-// 			schema: {
-// 				body: createProfileBodySchema,
-// 			},
-// 		},
-// 		async function (request, reply): Promise<ProfileEntity> {}
-// 	);
+			if (!profile) {
+				return reply.status(404).send({ message: Constants.PROFILE_ERROR });
+			}
 
-// 	fastify.delete(
-// 		'/:id',
-// 		{
-// 			schema: {
-// 				params: idParamSchema,
-// 			},
-// 		},
-// 		async function (request, reply): Promise<ProfileEntity> {}
-// 	);
+			return reply.send(profile);
+		}
+	);
 
-// 	fastify.patch(
-// 		'/:id',
-// 		{
-// 			schema: {
-// 				body: changeProfileBodySchema,
-// 				params: idParamSchema,
-// 			},
-// 		},
-// 		async function (request, reply): Promise<ProfileEntity> {}
-// 	);
-// };
+	// fastify.post(
+	// 	'/',
+	// 	{
+	// 		schema: {
+	// 			body: createProfileBodySchema,
+	// 		},
+	// 	},
+	// 	async function (request, reply): Promise<ProfileEntity> {}
+	// );
 
-// export default plugin;
+	// fastify.delete(
+	// 	'/:id',
+	// 	{
+	// 		schema: {
+	// 			params: idParamSchema,
+	// 		},
+	// 	},
+	// 	async function (request, reply): Promise<ProfileEntity> {}
+	// );
+
+	// fastify.patch(
+	// 	'/:id',
+	// 	{
+	// 		schema: {
+	// 			body: changeProfileBodySchema,
+	// 			params: idParamSchema,
+	// 		},
+	// 	},
+	// 	async function (request, reply): Promise<ProfileEntity> {}
+	// );
+};
+
+export default plugin;
