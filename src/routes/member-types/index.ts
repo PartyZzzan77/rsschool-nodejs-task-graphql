4778ec783d29b3ec80b874dd74444be58a113ff6
Mitya <partyzan327@gmail.com>
2023-01-24 23:29:52 +0300

    feat: implement member-types/get methods

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -1,35 +1,48 @@
-// import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-// import { idParamSchema } from '../../utils/reusedSchemas';
-// import { changeMemberTypeBodySchema } from './schema';
-// import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
+import { Constants } from './../../utils/constants';
+import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { idParamSchema } from '../../utils/reusedSchemas';
+//import { changeMemberTypeBodySchema } from './schema';
+import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
 
-// const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
-// 	fastify
-// ): Promise<void> => {
-// 	fastify.get('/', async function (request, reply): Promise<
-// 		MemberTypeEntity[]
-// 	> {});
+const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
+	fastify
+): Promise<void> => {
+	fastify.get('/', async function (request, reply): Promise<
+		MemberTypeEntity[]
+	> {
+		return reply.send(this.db.memberTypes);
+	});
 
-// 	fastify.get(
-// 		'/:id',
-// 		{
-// 			schema: {
-// 				params: idParamSchema,
-// 			},
-// 		},
-// 		async function (request, reply): Promise<MemberTypeEntity> {}
-// 	);
+	fastify.get(
+		'/:id',
+		{
+			schema: {
+				params: idParamSchema,
+			},
+		},
+		async function (request, reply): Promise<MemberTypeEntity> {
+			const { id } = request.params;
 
-// 	fastify.patch(
-// 		'/:id',
-// 		{
-// 			schema: {
-// 				body: changeMemberTypeBodySchema,
-// 				params: idParamSchema,
-// 			},
-// 		},
-// 		async function (request, reply): Promise<MemberTypeEntity> {}
-// 	);
-// };
+			const memberType = await this.db.memberTypes.findOne({ key: 'id', equals: id });
 
-// export default plugin;
+			if (!memberType) {
+				return reply.status(404).send({ message: Constants.MEMBER_TYPE_ERROR });
+			}
+
+			return reply.send(memberType);
+		}
+	);
+
+	// fastify.patch(
+	// 	'/:id',
+	// 	{
+	// 		schema: {
+	// 			body: changeMemberTypeBodySchema,
+	// 			params: idParamSchema,
+	// 		},
+	// 	},
+	// 	async function (request, reply): Promise<MemberTypeEntity> {}
+	// );
+};
+
+export default plugin;
