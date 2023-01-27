b8089d78d85daf61f175374c772fd978df91eddb
Mitya <partyzan327@gmail.com>
2023-01-27 22:14:26 +0300

    feat: add depth requests validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,12 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
-import { graphql } from 'graphql';
+import { graphql, parse } from 'graphql';
+import { validate } from 'graphql/validation';
 import { schema } from './schema';
+import depthLimit from 'graphql-depth-limit';
+
+const MAX_DEPTH = 6;
+const REQUEST_DEPTH_ERROR = 'Request limit exceeded';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 	fastify
@@ -16,22 +21,38 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 		async function (request, reply) {
 			const { query, variables } = request.body;
 
-			if (query) {
-				if (variables) {
+			try {
+				if (query) {
+					if (variables) {
+						const validateErrors = validate(
+							schema,
+							parse(query),
+							depthLimit(MAX_DEPTH)
+						);
+
+						if (validateErrors) {
+							throw new Error(REQUEST_DEPTH_ERROR);
+						}
+
+						const result = await graphql({
+							schema,
+							source: query,
+							variableValues: variables,
+						});
+
+						return reply.send(result);
+					}
+
 					const result = await graphql({
 						schema,
 						source: query,
-						variableValues: variables,
 					});
-
 					return reply.send(result);
 				}
-
-				const result = await graphql({
-					schema,
-					source: query,
-				});
-				return reply.send(result);
+			} catch (error) {
+				if (error instanceof Error) {
+					console.log(error.message);
+				}
 			}
 		}
 	);
